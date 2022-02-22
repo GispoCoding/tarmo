c530ee660488fd27a98fd7b23e3d89c14e03e99c
Mikael Vaaltola <mikael@vaalto.la>
2022-02-22 11:25:06 +0200

    Support automatic type generation for CSS modules

--- web/config/webpack.common.config.babel.js
+++ web/config/webpack.common.config.babel.js
@@ -53,7 +53,19 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        exclude: /\.module\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          "style-loader",
+          "@teamsupercell/typings-for-css-modules-loader",
+          {
+            loader: "css-loader",
+            options: { modules: true },
+          },
+        ],
       },
     ],
   },
