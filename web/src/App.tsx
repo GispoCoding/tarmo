ed9a409bde87310c7b24727c9d0e95a6d0bda3d3
Mikael Vaaltola <mikael@vaalto.la>
2022-02-23 16:59:26 +0200

    Add placeholder splash screen
    Fix #72.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,16 @@
 import * as React from "react";
 import Map from "./components/Map";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  return <Map />;
+  const [showSplash, setShowSplash] = useState(true);
+  useEffect(() => {
+    // Show splash screen for 2s on startup
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+  return showSplash ? <div>Welcome to Tarmo</div> : <Map />;
 }
