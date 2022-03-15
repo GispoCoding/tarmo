03dcc3aadb990ffb55c1db267e3c9ed162487f19
Anniina Kovalainen <anniina.kovalainen@gmail.com>
2022-03-15 13:43:34 +0200

    76 add responsiveness (#112)
    * Add responsiveness and fix slide button
    
    * Add responsive styling for tablet

--- web/src/components/InfoSlider.tsx
+++ web/src/components/InfoSlider.tsx
@@ -31,9 +31,11 @@ export default function InfoSlider({ popupInfo }: PopupProps) {
     </div>,
     <div key={3}>
       <h3>Lue lisää kunnan verkkosivuilta</h3>
-      <button>
-        <a href={popupInfo.properties["www"]}>Siirry verkkosivuille</a>
-      </button>
+      <div className="button-slide">
+        <a href={popupInfo.properties["www"]}>
+          <button>Siirry verkkosivuille</button>
+        </a>
+      </div>
     </div>,
   ];
 
