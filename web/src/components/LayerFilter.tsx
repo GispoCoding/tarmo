cea58fdafbf5a9b867cd7eb328e3b9b78d2ab176
anniinakovalainen <anniina.kovalainen@gmail.com>
2022-03-16 18:49:43 +0200

    Add styling and correct filters
    Slider not created!

--- web/src/components/LayerFilter.tsx
+++ web/src/components/LayerFilter.tsx
@@ -2,49 +2,68 @@ import * as React from "react";
 
 const categories = [
   {
+    name: "ulkoilureitit",
+    icon: "trekking-dark.png",
+  },
+  {
+    name: "ulkoiluaktiviteetit",
+    icon: "frisbee-dark.png",
+  },
+  {
+    name: "ulkoilupaikat",
+    icon: "park-dark.png",
+  },
+  {
+    name: "ruokailu",
+    icon: "campfire-dark.png",
+  },
+  {
+    name: "pyöräily",
+    icon: "cycling-dark.png",
+  },
+  {
     name: "hiihto",
     icon: "ski-dark.png",
+  },
+  {
+    name: "luistelu",
+    icon: "skating-dark.png",
   },
   {
     name: "uinti",
     icon: "swimming-dark.png",
   },
   {
-    name: "toilet",
-    icon: "toilet-dark.png",
+    name: "vesiulkoilu",
+    icon: "dinghy-dark.png",
   },
   {
-    name: "vaellus",
-    icon: "trekking-dark.png",
+    name: "nähtävyydet",
+    icon: "camera-dark.png",
   },
   {
-    name: "kävely",
-    icon: "walking-dark.png",
+    name: "pysäköinti",
+    icon: "info-dark.png",
   },
   {
-    name: "sauna",
-    icon: "sauna-dark.png",
-  },
-  {
-    name: "nuotio",
-    icon: "campfire-dark.png",
-  },
-  {
-    name: "ulkokuntosali",
-    icon: "barbell-dark.png",
+    name: "pysäkit",
+    icon: "info-dark.png",
   },
 ];
 
 export default function LayerFilter() {
   return (
-    <div>
-      {categories.map((category, idx) => (
-        <div className={`filter-item-${category.name}`} key={idx}>
-          <button style={{ backgroundImage: `url(img/${category.icon})` }}>
-            asd
-          </button>
-        </div>
-      ))}
+    <div className="layerfilter-wrapper">
+      <div className="layerfilter-container">
+        {categories.map((category, idx) => (
+          <div className={`layerfilter-item`} key={idx}>
+            <button
+              className={`layerfilter-item-${category.name}`}
+              style={{ backgroundImage: `url(img/${category.icon})` }}
+            ></button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
