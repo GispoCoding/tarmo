b5f9c382ecb7d09f7e70269765557ca3449ab5fd
Riku Oja <riku.oja@kapsi.fi>
2022-03-29 18:34:12 +0300

    Add correct parameters and schedules for nightly imports

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -22,7 +22,6 @@ resource "aws_cloudwatch_log_group" "lambda_wfs" {
   tags              = local.default_tags
 }
 
-# not tested yet, probably won't work
 resource "aws_cloudwatch_event_rule" "lambda_lipas" {
   name        = "Tarmo-lambda-lipas-update"
   description = "Run lipas import every night"
@@ -30,9 +29,36 @@ resource "aws_cloudwatch_event_rule" "lambda_lipas" {
   tags              = local.default_tags
 }
 
-# not tested yet, probably won't work
 resource "aws_cloudwatch_event_target" "lambda_lipas" {
   target_id = "load_lipas"
   rule      = aws_cloudwatch_event_rule.lambda_lipas.name
   arn       = aws_lambda_function.lipas_loader.arn
+  input     = "{\"close_to_lon\": 23.7634608, \"close_to_lat\": 61.4976505, \"radius\": 100}"
+}
+
+resource "aws_cloudwatch_event_rule" "lambda_wfs" {
+  name        = "Tarmo-lambda-wfs-update"
+  description = "Run wfs import every night"
+  schedule_expression = "cron(15 4 * * ? *)"
+  tags              = local.default_tags
+}
+
+resource "aws_cloudwatch_event_target" "lambda_wfs" {
+  target_id = "load_wfs"
+  rule      = aws_cloudwatch_event_rule.lambda_wfs.name
+  arn       = aws_lambda_function.wfs_loader.arn
+}
+
+resource "aws_cloudwatch_event_rule" "lambda_osm" {
+  name        = "Tarmo-lambda-osm-update"
+  description = "Run osm import every night"
+  schedule_expression = "cron(30 4 * * ? *)"
+  tags              = local.default_tags
+}
+
+resource "aws_cloudwatch_event_target" "lambda_osm" {
+  target_id = "load_osm"
+  rule      = aws_cloudwatch_event_rule.lambda_osm.name
+  arn       = aws_lambda_function.osm_loader.arn
+  input     = "{\"close_to_lon\": 23.7634608, \"close_to_lat\": 61.4976505, \"radius\": 20}"
 }
