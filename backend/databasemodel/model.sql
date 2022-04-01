034b9f9c46ac9353bc6d24fa98405c7e860f735e
pavetsu14 <55135974+pavetsu14@users.noreply.github.com>
2022-04-01 13:31:19 +0300

    Luonnonsuojelualueet (#154)
    * Add tables for storing nature conservation areas
    
    * Added one file which dropped off from last commit
    
    * Add luonnonsuojelualueet tables to kooste counts
    
    Co-authored-by: Riku Oja <Rikuoja@users.noreply.github.com>

--- backend/databasemodel/model.sql
+++ backend/databasemodel/model.sql
@@ -1549,6 +1549,44 @@ CREATE TABLE kooste.museovirastoarcrest_muinaisjaannokset (
 ALTER TABLE kooste.museovirastoarcrest_muinaisjaannokset OWNER TO tarmo_admin;
 -- ddl-end --
 
+-- object: kooste.syke_natura2000 | type: TABLE --
+-- DROP TABLE IF EXISTS kooste.syke_natura2000 CASCADE;
+CREATE TABLE kooste.syke_natura2000 (
+	"naturaTunnus" text NOT NULL,
+	geom geometry(MULTIPOINT, 4326) NOT NULL,
+	name text NOT NULL,
+	tarmo_category text DEFAULT 'Tausta-aineistot',
+	type_name text DEFAULT 'Natura-alue',
+	"paatosPvm" date,
+	"luontiPvm" date,
+	"muutosPvm" date,
+	"paattymisPvm" date,
+	CONSTRAINT syke_natura2000_pk PRIMARY KEY ("naturaTunnus")
+);
+-- ddl-end --
+ALTER TABLE kooste.syke_natura2000 OWNER TO tarmo_admin;
+-- ddl-end --
+
+-- object: kooste.syke_valtionluonnonsuojelualueet | type: TABLE --
+-- DROP TABLE IF EXISTS kooste.syke_valtionluonnonsuojelualueet CASCADE;
+CREATE TABLE kooste.syke_valtionluonnonsuojelualueet (
+	"LsAlueTunnus" text NOT NULL,
+	geom geometry(MULTIPOINT, 4326) NOT NULL,
+	name text NOT NULL,
+	tarmo_category text DEFAULT 'Tausta-aineistot',
+	type_name text DEFAULT 'Valtion luonnonsuojelualue',
+	"infoFi" text,
+	"PaatPvm" date,
+	"PaatNimi" text,
+	"VoimaantuloPvm" date,
+	"MuutosPvm" date,
+	"LakkautusPvm" date,
+	CONSTRAINT syke_valtionluonnonsuojelualueet_pk PRIMARY KEY ("LsAlueTunnus")
+);
+-- ddl-end --
+ALTER TABLE kooste.syke_valtionluonnonsuojelualueet OWNER TO tarmo_admin;
+-- ddl-end --
+
 -- object: kooste.metadata | type: TABLE --
 -- DROP TABLE IF EXISTS kooste.metadata CASCADE;
 CREATE TABLE kooste.metadata (
@@ -1923,6 +1961,22 @@ GRANT SELECT,INSERT,UPDATE,DELETE
    ON TABLE kooste.museovirastoarcrest_muinaisjaannokset
    TO tarmo_read_write;
 -- ddl-end --
+
+GRANT SELECT
+   ON TABLE kooste.syke_natura2000
+   TO tarmo_read;
+
+GRANT SELECT,INSERT,UPDATE,DELETE
+   ON TABLE kooste.syke_natura2000
+   TO tarmo_read_write;
+
+GRANT SELECT
+   ON TABLE kooste.syke_valtionluonnonsuojelualueet
+   TO tarmo_read;
+
+GRANT SELECT,INSERT,UPDATE,DELETE
+   ON TABLE kooste.syke_valtionluonnonsuojelualueet
+   TO tarmo_read_write;
 
 -- object: grant_rawd_5082180c53 | type: PERMISSION --
 GRANT SELECT,INSERT,UPDATE,DELETE
