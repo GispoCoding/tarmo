28011528819138251fe381a27564c0058d905d33
Eetu Nisula <eetu.nisula@metatavu.fi>
2026-09-02 16:42:36 +0300

    Upgrade frontend to React 18

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,18 @@
 import { CssBaseline, responsiveFontSizes, ThemeProvider } from "@mui/material";
 import * as React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import theme from "./theme/theme";
 
-render(
+const container = document.getElementById("map");
+
+if (!container) {
+  throw new Error("Map container was not found");
+}
+
+createRoot(container).render(
   <ThemeProvider theme={responsiveFontSizes(theme)}>
     <CssBaseline />
     <App />
-  </ThemeProvider>,
-  document.getElementById("map")
+  </ThemeProvider>
 );
